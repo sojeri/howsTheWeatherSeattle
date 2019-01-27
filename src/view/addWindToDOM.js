f0b8cc12dfaef2fcd0cff5df455b5c37753f1cbe
Jeri Sommers 👾 <sojeri@uw.edu>
2019-01-27 12:36:58 -0600

    refactor wind handling a bit

--- src/view/addWindToDOM.js
+++ src/view/addWindToDOM.js
@@ -1,31 +1,43 @@
 const addClass = require('./addClass');
 const getCardinalWindDirection = require('./utils/getCardinalWindDirection');
 
-function addWindToDOM(weatherElement, wind) {
-    let windSpeed = wind.speed;
-
-    // TODO: add sideways falling objects if windSpeed > 30?
-    if (windSpeed > 20) {
+function addWindSpeed(weatherElement, speed, dataUpdateOnly) {
+    document.getElementById('wind-speed').innerHTML = speed;
+    if (dataUpdateOnly) return;
+    
+    // TODO: styles-- add sideways falling objects if windSpeed > 30 (extreme)?
+    if (speed > 20) {
         addClass(weatherElement, 'wind-high');
-    } else if (windSpeed > 10) {
+    } else if (speed > 10) {
         addClass(weatherElement, 'wind-med');
-    } else if (windSpeed > 0) {
+    } else if (speed > 0) {
         addClass(weatherElement, 'wind-low');
     }
 
-    document.getElementById('wind-speed').innerHTML = windSpeed;
-    
-    let windDirection = getCardinalWindDirection(wind.deg);
-    if (windDirection) { // this property is not guaranteed to be returned by the API
-        if (windDirection.indexOf('W') > -1) {
-            addClass(weatherElement, 'wind-west');
-        } else if (windDirection.indexOf('E') > -1) {
-            addClass(weatherElement, 'wind-east');
-        }
-        
-        document.getElementById('wind-direction').innerHTML = windDirection;
+}
+
+function addWindDirection(weatherElement, windDegrees, dataUpdateOnly) {
+    let direction = getCardinalWindDirection(windDegrees);
+    document.getElementById('wind-direction').innerHTML = direction;
+    if (dataUpdateOnly) return;
+
+    if (direction && direction.indexOf('W') > -1) {
+        addClass(weatherElement, 'wind-west');
+    } else {
+        addClass(weatherElement, 'wind-east');
     }
 
+}
+
+function addWindToDOM(weatherElement, wind) {
+    if (!wind.speed && wind.speed !== 0) {
+        document.getElementByClass('wind').innerHTML = '';
+        return;
+    }
+
+    const isDataOnly = !wind.speed || wind.speed == 0;
+    addWindSpeed(weatherElement, wind.speed, isDataOnly);
+    addWindDirection(weatherElement, wind.deg, isDataOnly);
     require('./styles/wind.scss');
 }
 
