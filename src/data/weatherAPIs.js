4873027242c8d0ac877034bdc112a6dbe315c700
Jeri Sommers 👾 <sojeri@uw.edu>
2019-01-30 11:37:36 -0600

    moon should be visible between moon rise & set

--- src/data/weatherAPIs.js
+++ src/data/weatherAPIs.js
@@ -4,7 +4,7 @@ const SEATTLE_LONG = '-122.3205388';
 // https://openweathermap.org/current
 const WEATHER_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?lat=${SEATTLE_LAT}&lon=${SEATTLE_LONG}&units=imperial&appid=231512774f62e8fcb7d1a19af041b94d`;
 const FALLBACK_WEATHER = {
-    weather: [{ id: 501 }],
+    weather: [{ id: 301 }],
     wind: { speed: 10, deg: 90 },
     main: {
         humidity: 84,
@@ -19,7 +19,11 @@ const FALLBACK_WEATHER = {
 // https://solunar.org/#usage
 const REPLACE = '@@REPLACE@@';
 const MOON_ENDPOINT = `https://api.solunar.org/solunar/${SEATTLE_LAT},${SEATTLE_LONG},${REPLACE},-7`
-const FALLBACK_MOON = { moonPhase: 'Waxing Crescent', };
+const FALLBACK_MOON = {
+    moonPhase: 'Waning Gibbous',
+    moonRise: '01:04',
+    moonSet: '16:53',
+}; 
 
 module.exports = {
     WEATHER_ENDPOINT,
--- src/view/addMoonToDOM.js
+++ src/view/addMoonToDOM.js
@@ -1,46 +1,73 @@
 const addClass = require('./addClass');
 
-function addMoonToDOM(moonBlob) {
-    let shapeClass, lightStartClass;
-    switch (moonBlob.moonPhase) {
-        case 'New Moon':
-            shapeClass = 'empty';
-            break;
+const convertStringNumberToNumber = sn => Number(sn);
+
+// FIXME: this logic may be broken. verify API returns always: current moon rise & set always in that order
+//        (eg, never previously set at 04:30 and back up at 21:00 when current time is 16:00 same day)
+function isMoonVisible(moonRiseTime, moonSetTime) {
+    let time = new Date(Date.now());
+    let currentHour = time.getUTCHours() - 7;
+    if (currentHour < 1) currentHour += 24;
+    const currentMin = time.getUTCMinutes();
+    
+    let [riseHour, riseMin] = moonRiseTime.split(':').map(convertStringNumberToNumber);
+    let [setHour, setMin] = moonSetTime.split(':').map(convertStringNumberToNumber);
+
+    if (currentHour < riseHour) currentHour += 24;
+    if (setHour < riseHour) setHour += 24;
+
+    if (riseHour < currentHour && currentHour < setHour) return true;
+    if (riseHour == currentHour && riseMin <= currentMin) return true;
+    if (setHour == currentHour && currentMin <= setMin) return true;
+
+    return false;
+}
+
+const phases = {
+    new: { shapeClass: 'new', },
+    waxingCrescent: { shapeClass: 'crescent', lightStartClass: 'right', },
+    firstQuarter: { shapeClass: 'half', lightStartClass: 'right', },
+    waxingGibbous: { shapeClass: 'gibbous', lightStartClass: 'right', },
+    full: { shapeClass: 'full', },
+    waningGibbous: { shapeClass: 'gibbous', lightStartClass: 'left', },
+    thirdQuarter: { shapeClass: 'half', lightStartClass: 'left', },
+    waningCrescent: { shapeClass: 'crescent', lightStartClass: 'left', },
+}
+
+function getPhase(blob) {
+    switch (blob.moonPhase) {
         case 'Waxing Crescent':
-            shapeClass = 'crescent';
-            lightStartClass = 'right';
-            break;
+            return blob.moonIllumination < .10 ? phases.new : phases.waxingCrescent;
         case 'First Quarter':
-            shapeClass = 'half';
-            lightStartClass = 'right';
-            break;
+            return blob.moonIllumination < .35 ? phases.waxingCrescent : phases.firstQuarter;
         case 'Waxing Gibbous':
-            shapeClass = 'gibbous';
-            lightStartClass = 'right';
-            break;
+            return blob.moonIllumination < .60 ? phases.firstQuarter : phases.waxingGibbous;
         case 'Full Moon':
-            shapeClass = 'full';
-            break;
+            return blob.moonIllumination < .85 ? phases.waxingGibbous : phases.full;
         case 'Waning Gibbous':
-            shapeClass = 'gibbous';
-            lightStartClass = 'left';
-            break;
+            return blob.moonIllumination > .90 ? phases.full : phases.waningGibbous;
         case 'Third Quarter':
-            shapeClass = 'half';
-            lightStartClass = 'left';
-            break;
+            return blob.moonIllumination > .65 ? phases.waningGibbous : phases.thirdQuarter;
         case 'Waning Crescent':
-            shapeClass = 'crescent';
-            lightStartClass = 'left';
-            break;
+            return blob.moonIllumination > .40 ? phases.thirdQuarter : phases.waningCrescent;
+        case 'New Moon':
+            return blob.moonIllumination > .15 ? phases.waningCrescent : phases.new;
         default:
             throw new Error('unrecognized moon phase');
     }
+}
 
-    let moonElement = document.getElementById('moon');
+function addMoonToDOM(moonBlob) {
+    
+    if (isMoonVisible(moonBlob.moonRise, moonBlob.moonSet)) {
+        let moonElement = document.getElementById('moon');
+        addClass(moonElement, 'visible');
 
-    addClass(moonElement, shapeClass);
-    if (lightStartClass) addClass(moonElement, lightStartClass);
+        let { shapeClass, lightStartClass } = getPhase(moonBlob);
+        addClass(moonElement, shapeClass);
+        if (lightStartClass) addClass(moonElement, lightStartClass);
+    }
+
 }
 
 module.exports = addMoonToDOM;
